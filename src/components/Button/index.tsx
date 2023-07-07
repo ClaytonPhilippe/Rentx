03934ce13aa10cf2eefcef0dd1d512f991f2a083
unknown <claytonphilippe.com>
2023-07-07 10:57:10 -0300

    rentx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
 import { useTheme } from 'styled-components';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 import {
   Container,
   Title
 } from './styles';
 
-interface Props {
+interface Props extends RectButtonProps {
   title: string;
   color?: string;
-  onPress: () => void;
-  enable?: boolean;
   loading?: boolean;
+  light?: boolean;
 }
 
 export function Button({
   title,
   color,
   onPress,
-  enable = true,
+  enabled = true,
   loading = false,
+  light = false
 }: Props) {
   const theme = useTheme();
 
@@ -28,13 +29,13 @@ export function Button({
     <Container
       color={color ? color : theme.colors.main}
       onPress={onPress}
-      enabled={enable}
-      style={{ opacity: (enable === false || loading === true) ? .5 : 1 }}
+      enabled={enabled}
+      style={{ opacity: (enabled === false || loading === true) ? .5 : 1 }}
     >
       {
         loading
           ? <ActivityIndicator color={theme.colors.shape} />
-          : <Title>{title}</Title>
+          : <Title light={light}>{title}</Title>
       }
     </Container>
   );
